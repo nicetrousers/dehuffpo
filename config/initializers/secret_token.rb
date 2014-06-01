0e8323312690ff8ea3bb22ecb8f2b11bd870b176
scribacious <keithlaidlaw@gmail.com>
2014-06-01 09:05:46 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dehuffpo::Application.config.secret_token = '10ad6fc0ff2f2fcc102648ddb74a67e4c15c940e6a10424c08b081d6957ba8ea317d46588bf952aa7315b911e3b9f7f1ae5012d078a0f3a2f420a127bf0390d4'
